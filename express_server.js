3c9093423b7a2e3a4369e494ba1ed8148218a9c6
chrynuik <catherine.hrynuik@gmail.com>
2017-11-08 23:56:44 +0000

    added cookies package

--- express_server.js
+++ express_server.js
@@ -2,10 +2,13 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080;
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser')
 
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
+
+app.use(cookieParser());
 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
